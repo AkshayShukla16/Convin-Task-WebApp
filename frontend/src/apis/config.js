349d089002503501bd86f025f48a1f9b9e6c9628
AKSHAY SHUKLA <98264215+AkshayShukla16@users.noreply.github.com>
2023-03-27 17:09:26 +0530

    Update config.js

--- frontend/src/apis/config.js
+++ frontend/src/apis/config.js
@@ -1,5 +1,5 @@
 //export const base_url = `http://localhost:9002`;
 
-export const base_url = `https://convin-fa-backend.vercel.app`;
+ // export const base_url = `https://convin-fa-backend.vercel.app`;
 
-//export const base_url= `https://convin-task-qh2a.onrender.com`
+export const base_url= `https://convin-task-qh2a.onrender.com`
