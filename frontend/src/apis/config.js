43017d536d8fdf9944c636a317a0a2e9adcd5fd1
AKSHAY SHUKLA <98264215+AkshayShukla16@users.noreply.github.com>
2023-03-27 17:12:24 +0530

    updated sever url

--- frontend/src/apis/config.js
+++ frontend/src/apis/config.js
@@ -1,5 +1,7 @@
-//export const base_url = `http://localhost:9002`;
+
+//export const base_url = `http://localhost:9002`;  //for locally checking
+
+export const base_url= `https://convin-task-qh2a.onrender.com`;  //server uploaded on render
+
 
  // export const base_url = `https://convin-fa-backend.vercel.app`;
-
-export const base_url= `https://convin-task-qh2a.onrender.com`
